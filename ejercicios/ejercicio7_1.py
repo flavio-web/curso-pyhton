3c2b64685bcf21e2f1e20237130340991ea95b1c
Flavio Roman <flavioroman@192.168.1.46>
2025-04-10 22:17:41 -0500

    fechas en phyton

--- ejercicios/ejercicio7_1.py
+++ ejercicios/ejercicio7_1.py
@@ -4,7 +4,8 @@
 class Personaje:
 
     __personajes = []
-    def __init__(self, nombre, fuerza, velocidad):
+        
+    def newPersonaje(self, nombre, fuerza, velocidad):
         self.nombre = nombre
         self.fuerza = fuerza
         self.velocidad = velocidad
@@ -19,6 +20,13 @@ class Personaje:
         nueva_velocidad =  min((((self.velocidad + otro_personaje.velocidad) / 2) * 1.2), 100)
 
         return Personaje(nuevo_nombre, nueva_fuerza, nueva_velocidad)
+    
+    def existePersonaje(self, nombrePersonaje):
+        for personaje in self.__personajes:
+            if personaje.nombre.lower() == nombrePersonaje.lower():
+                return True
+            
+        return False
 
     @property
     def personajes(self):
@@ -26,9 +34,38 @@ class Personaje:
     
     
     
-goku = Personaje('Goku', 80, 60)
-vegeta = Personaje('Vegeta', 50, 75)
+print("*** Menu de opciones ***\n 1.Registrar personaje\n 2.Fusionar personajes\n 3.listar Personajes")
 
-gogeta = goku + vegeta
-print(gogeta)
-print(gogeta.personajes)
+while True:
+    option = int(input('Ingrese una opcion del menu: '))
+    if( option >= 1 | option <= 3):
+        break        
+    print("Error. La opcion selecionada es incorrecta")   
+    
+print("genial saliste del bucle")
+
+personaje = Personaje()
+
+if option == 1:
+    nombrePersonaje = ""
+    while True:
+        nombrePersonaje = input('Ingrese el nombre del personaje:')
+        existe = personaje.existePersonaje( nombrePersonaje )
+        if( existe == False ):
+            break
+        print(f"El personaje {nombrePersonaje} ya existe, ingrese otro nombre de personaje")
+        
+    fuerzaPersonaje = float(input("Ingrese el valor del fuerza del personaje entre 0 - 100:"))
+    velocidadPersonaje = float(input("Ingrese el valor de velocidad del personaje entre 0 - 100:"))
+    
+    personaje.newPersonaje(nombrePersonaje, velocidadPersonaje, fuerzaPersonaje)
+    print(f"Personaje {nombrePersonaje} registrado correctamente")
+    print(personaje.personajes)
+    
+    
+# goku = Personaje('Goku', 80, 60)
+# vegeta = Personaje('Vegeta', 50, 75)
+
+# gogeta = goku + vegeta
+# print(gogeta)
+# print(gogeta.personajes)
--- fechas_horas.py
+++ fechas_horas.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 #1. Obtener la fecha y hora actual
 now = datetime.now()
@@ -11,3 +11,27 @@ print(f"Fecha y hora especifica: {fecha_especifica}")
 #3. formatear fechas
 formato = now.strftime('%d/%m/%Y %H:%M:%S')
 print(f"Fecha y hora formateada: {formato}")
+
+#4. Operaciones con fechas
+yesterday = datetime.now() - timedelta(days=1)
+print(f"Ayer: {yesterday}")
+
+tomorrow = datetime.now() + timedelta(hours=1)
+print(f"Tomoroow: {tomorrow}")
+
+#5. obtener componentes individuales de una fecha
+year = datetime.now().year
+print(f"Anio: {year}")
+
+#6.calcular la diferencia entre dos fechas
+date1 = datetime.now()
+date2 = datetime(2025, 4, 2)
+difference = date2 - date1
+print(f"Diferencia: {difference}")
+print(f"Diferencia days: {difference.days}")
+
+#importar el locale para poner los nombres de las fechas es espaniol
+import locale
+locale.setlocale(locale.LC_TIME, 'es_ES.UTF-8')
+formate_date = now.strftime("%A %B %Y %H:%M:%S")
+print(f"fecha formateada al espaniol: {formate_date}")
